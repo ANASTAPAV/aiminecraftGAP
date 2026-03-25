7d5ba1823c066d84700cad93d41152e85831abf2
ANASTAPAV <gapavlidis2013@gmail.com>
2026-03-25 19:40:00 +0200

    Create main.js file for main game entry point and initialization

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// Main game entry point for AIMinecraftGAP
+
+function initializeGame() {
+    console.log('Game has been initialized.');
+    // You can add more initialization code here.
+}
+
+// Start the game
+initializeGame();
