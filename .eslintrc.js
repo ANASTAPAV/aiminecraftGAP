4cbcab925f67d2f5edc39c3e49fbfde78abb0518
ANASTAPAV <gapavlidis2013@gmail.com>
2026-03-25 19:43:50 +0200

    Create .eslintrc.js file with eslint configuration for browser, node, and jest environments

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    // Add your custom rules here
+  },
+};
